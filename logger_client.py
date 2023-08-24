f205b6a31908637010fa49cc3f3cc48383563619
PavelProjects <paveloalexeev52@gmail.com>
2023-08-25 00:11:27 +0300

    added colors for different levels of log messages

--- logger_client.py
+++ logger_client.py
@@ -5,8 +5,10 @@ import struct
 from datetime import datetime
 
 LOGGER_FILE = "logger.log"
+START_COLOR = "\033["
+END_COLOR = "\033[0m"
 
-ipColor = {}
+ipNameColor = {}
 group = ("224.1.1.1", 7779)
 lastColorIndex = 2
 
@@ -18,16 +20,31 @@ mainSocket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
 logFile = open(LOGGER_FILE, "a")
 
+def colorByMessageType(messageType):
+    if messageType == "ERROR":
+        return "31m"
+    if messageType == "WARNING":
+        return "35m"
+    if messageType == "INFO":
+        return "34m"
+    if messageType == "DEBUG":
+        return "32m"
+    return "30"
+
 try:
     while True:
         message = mainSocket.recv(4096).decode()
-        ip = message[message.index('{'):message.index('}')]
+        # that will cause problems
+        ipName = message[message.index('{'):message.index('}')+1]
+        messageType = message[message.index('}[') + 2:message.index('][')]
+        messageCuted = message[message.index('}[') + 1:]
 
-        if (ip not in ipColor.keys()):
-            ipColor.update({ip: f"\033[9{lastColorIndex}m"})
+        if (ipName not in ipNameColor.keys()):
+            ipNameColor.update({ipName: f"3;9{lastColorIndex}m"})
             lastColorIndex += 1
 
-        formatedMessage = f"{ipColor[ip]}[{datetime.now()}]{message}\033[0m"
+        formatedMessage = f"[{datetime.now()}]{START_COLOR + ipNameColor[ipName]}{ipName}{END_COLOR}"
+        formatedMessage += f"{START_COLOR + colorByMessageType(messageType)}{messageCuted}{END_COLOR}"
         print(formatedMessage)
         logFile.write(formatedMessage + '\n')
         logFile.flush()

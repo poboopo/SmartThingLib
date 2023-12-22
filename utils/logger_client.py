d109ebcc0cc6424d39b7a48a54e8940e689c3ae8
PavelProjects <paveloalexeev52@gmail.com>
2023-12-22 21:43:32 +0300

    added tcp impl in logger client

--- utils/logger_client.py
+++ utils/logger_client.py
@@ -3,6 +3,9 @@
 import socket
 import struct
 from datetime import datetime
+import threading
+
+# connect to udp via param
 
 TCP = True
 PORT = 7779
@@ -13,22 +16,6 @@ END_COLOR = "\033[0m"
 
 ipColor = {}
 lastColorIndex = 2
-
-if (TCP):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.bind(("192.168.2.114", PORT))
-    sock.listen()
-    print("Waiting for connection...")
-    conn, addr = sock.accept()
-    print(f"Got connection from {addr}")
-else:
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind(("224.1.1.1", PORT))
-    mreq = struct.pack("4sl", socket.inet_aton(group[0]), socket.INADDR_ANY)
-    sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
-    conn = sock
-
 logFile = open(LOGGER_FILE, "a")
 
 def colorByLevel(logLevel):
@@ -42,41 +29,78 @@ def colorByLevel(logLevel):
         return "32m"
     return "30"
 
-try:
+def udp():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind(("224.1.1.1", PORT))
+    mreq = struct.pack("4sl", socket.inet_aton(group[0]), socket.INADDR_ANY)
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+    printHeader()
+    try:
+        recvMessages(sock)
+    except KeyboardInterrupt:
+        print("leaving...")
+    finally:
+        sock.close()
+
+def tcp():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(("192.168.2.114", PORT))
+    sock.listen()
+    print("Waiting for connection...")
+    printHeader()
+    try:
+        while(True):
+            conn, addr = sock.accept()
+            th = threading.Thread(target=recvMessages, args=[conn], daemon=True)
+            th.start()
+    except KeyboardInterrupt:
+        print("leaving...")
+    finally:
+        sock.close()
+
+def printHeader():
     print(f"{START_COLOR + '1;36m'}{'':->115}")
     print(f"{'TIMESTAMP': ^26} [{'IP': ^15} :: {'NAME': ^15}] - [{'LEVEL': ^7}] [{'TAG': ^20}] :: LOG MESSAGE")
     print(f"{'':-<115}{END_COLOR}")
-    while True:
-        try:
-            data = conn.recv(4096)
-            if (not data):
-                print("Connection closed")
-                break
-            message = data.decode()
-            splitted = message.split("_&_")
-            if (len(splitted) == 1):
-                print(message)
-                continue
 
-            ip = splitted[0]
-            name = splitted[1]
-            logLevel = splitted[2]
-            tag = splitted[3]
-            messageCuted = splitted[4].strip()
+def recvMessages(conn):
+    global ipColor
+    global lastColorIndex
+    
+    try:
+        while True:
+            try:
+                data = conn.recv(4096)
+                if (not data):
+                    print("Connection closed")
+                    break
+                message = data.decode()
+                splitted = message.split("_&_")
+                if (len(splitted) == 1):
+                    print(message)
+                    continue
 
-            if (ip not in ipColor.keys()):
-                ipColor.update({ip: f"3{lastColorIndex}m"})
-                lastColorIndex += 1
+                ip = splitted[0]
+                name = splitted[1]
+                logLevel = splitted[2]
+                tag = splitted[3]
+                messageCuted = splitted[4].strip()
 
-            formatedMessage = f"{START_COLOR + ipColor[ip]}{datetime.now()} [{ip: ^15} :: {name: ^15}]{END_COLOR} - "
-            formatedMessage += f"{START_COLOR + colorByLevel(logLevel)}[{logLevel: ^7}] [{tag: ^20}] :: {messageCuted}{END_COLOR}"
-            print(formatedMessage)
-            logFile.write(formatedMessage + '\n')
-            logFile.flush()
-        except Exception as e:
-            print("Failed to process message")
-except KeyboardInterrupt:
-    print("leaving...")
-finally:
-    sock.close()
+                if (ip not in ipColor.keys()):
+                    ipColor.update({ip: f"3{lastColorIndex}m"})
+                    lastColorIndex += 1
+
+                formatedMessage = f"{START_COLOR + ipColor[ip]}{datetime.now()} [{ip: ^15} :: {name: ^15}]{END_COLOR} - "
+                formatedMessage += f"{START_COLOR + colorByLevel(logLevel)}[{logLevel: ^7}] [{tag: ^20}] :: {messageCuted}{END_COLOR}"
+                print(formatedMessage)
+                logFile.write(formatedMessage + '\n')
+                logFile.flush()
+            except Exception as e:
+                print(f"Failed to process message: {e}")
+    finally:
+        conn.close()
+
+if __name__ == "__main__":
+    tcp()
     logFile.close()

9f65fae0c30bd9ec89d1347115d7c5713fbbac1c
PavelProjects <pavelprojects52@gmail.com>
2025-01-16 18:22:01 +0300

    added features docs; hide wifi password

--- src/Features.h
+++ src/Features.h
@@ -20,19 +20,23 @@
   #endif
 #endif
 
+// Enable actions
 #ifndef ENABLE_ACTIONS
   #define ENABLE_ACTIONS 1
 #endif
 
+// Enable number sensors
 #ifndef ENABLE_NUMBER_SENSORS
   #define ENABLE_NUMBER_SENSORS 1
 #endif
 
+// Enable text sensors
 #ifndef ENABLE_TEXT_SENSORS
   #define ENABLE_TEXT_SENSORS 1
 #endif
 
 #if ENABLE_NUMBER_SENSORS || ENABLE_TEXT_SENSORS
+  // Enable hooks for sensors
   #ifndef ENABLE_HOOKS
     #define ENABLE_HOOKS 1
   #endif
@@ -41,6 +45,7 @@
 #endif
 
 #if ENABLE_ACTIONS
+  // Enable action call scheduler
   #ifndef ENABLE_ACTIONS_SCHEDULER
     #define ENABLE_ACTIONS_SCHEDULER 1
   #endif
@@ -48,14 +53,17 @@
   #define ENABLE_ACTIONS_SCHEDULER 0
 #endif
 
+// Enable configuration
 #ifndef ENABLE_CONFIG
   #define ENABLE_CONFIG 1
 #endif
 
+// Enable ArduinoOTA
 #ifndef ENABLE_OTA
   #define ENABLE_OTA 1
 #endif
 
+// Enable logger
 #ifndef ENABLE_LOGGER
   #define ENABLE_LOGGER 1
 #endif
